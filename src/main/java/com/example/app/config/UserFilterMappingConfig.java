c86ec0b01d05cd10ee1e40d9b5c1acbfa3c571d7
hyuna0121 <teng3156@naver.com>
2025-12-03 15:34:05 +0900

    filter, intercept

---
+++ src/main/java/com/example/app/config/UserFilterMappingConfig.java
@@ -0,0 +1,24 @@
+package com.example.app.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.example.app.filters.UserFilter;
+
+import jakarta.servlet.Filter;
+
+@Configuration
+public class UserFilterMappingConfig implements WebMvcConfigurer {
+
+//	@Bean
+//	FilterRegistrationBean<Filter> filterRegistrationBean() {
+//		FilterRegistrationBean<Filter> f = new FilterRegistrationBean<>();
+//		f.setFilter(new UserFilter());
+//		f.addUrlPatterns("/qna/*", "/notice/add");
+//		
+//		return f;
+//	}
+	
+}
